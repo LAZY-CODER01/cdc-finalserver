8028d8348762a3ff250e9632a8458ce1a5d5eedf
LAZY-CODER01 <avinashmaurya430@gmail.com>
2024-12-13 14:54:07 +0530

    back changes

--- routes/team.js
+++ routes/team.js
@@ -190,27 +190,7 @@ router.get('/:teamName', authMiddleware, async (req, res) => {
     res.status(500).json({ message: 'An error occurred', error: err.message });
   }
 });
-
-// Fetch teamid details by teamName
-// router.get('/:teamName', authMiddleware, async (req, res) => {
-//   try {
-//     const { teamName } = req.params;
-
-//     // Find the team by name and populate member details
-//     const team = await Team.findOne({ name: teamName }).populate(
-//       'members',
-//       'name email phone universityRollNo codeforceHandle'
-//     );
-
-//     if (!team) {
-//       return res.status(404).json({ message: 'Team not found' });
-//     }
-
-//     res.status(200).json({ team });
-//   } catch (err) {
-//     res.status(500).json({ message: 'An error occurred', error: err.message });
-//   }
-// });
+// Fetch team details by teamId
 router.get('/:teamId', authMiddleware, async (req, res) => {
   try {
     const { teamId } = req.params;
@@ -227,6 +207,7 @@ router.get('/:teamId', authMiddleware, async (req, res) => {
     res.status(500).json({ message: 'An error occurred', error: err.message });
   }
 });
+
 router.post('/paymentStatus', authMiddleware, async (req, res) => {
   try {
     const { teamId, paymentStatus } = req.body;
